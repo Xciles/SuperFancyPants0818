bfb49ac116c1173e404d8892959565354cad7d49
Xciles <xciles@gmail.com>
2018-09-04 11:13:51 +0200

    Adding mvc project

---
+++ SuperFancyPants.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperFancyPants.Web.Data
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
